32a9cd8bad12570d44830b2abcb0591864b700ce
dpranata3 <donny.pranata3@gmail.com>
2019-04-10 10:13:15 +0700

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from '../config/axios'
+
+
+export const onRegister = (name, age, email, password) => {
+    return dispatch => {
+        axios.post('/users', {
+                name, age, email, password
+        }).then(res => {
+            console.log("YEaaaayyy");
+            
+        }).catch(e => {
+            console.log(e.response.data.replace('User validation failed: ', ''));
+        })
+    }
+}
+
+// export const onLogin = (email, password) => {
+
+// }
